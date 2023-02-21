07c08fd0831470b23a298b111293aef2ca7e51fb
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-21 11:55:07 +0100

    footer + section info-foot con vue

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 settingVue = Vue.createApp({
     data() {
         return {
+            // navbar nell'head
             navbar: {
                 logo: "./img/Logo.png",
                 link: [
@@ -14,8 +15,42 @@ settingVue = Vue.createApp({
                     "CONTACT US",
                     "SHOP",
                 ],
+            },
+
+            // footer
+            foot: {
+
+                // index 0
+                colFoot: [
+                    "502 New Design Str,",
+                    "Melbourne, San Francisco,",
+                    "CA 94110, United States Of",
+                    "America Australia",
+                    "T:1 123-435-789",
+                    "Email:demo@example.com",
+                ],
+
+                // index 1
+                colFoot: [
+                    "Product Support",
+                    "Checkout",
+                    "Report Abuse",
+                    "Readeem Voucher",
+                    "Order Status",
+                ],
+
+                // index 2
+                colFoot: [
+                    "Policies & Rules",
+                    "Privacy Policy",
+                    "License Policy",
+                    "My Account",
+                    "Locality",
+                ],
+                copyright: "@2023 All Rigths Reserved Developed By TemplateMela"
 
             }
+
         }
     },
 })
