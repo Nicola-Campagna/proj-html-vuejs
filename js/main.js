9d5982abecb0690e62fabc8c584996184fbb199f
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-21 11:12:13 +0100

    navbar con link in vue

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@
 settingVue = Vue.createApp({
     data() {
         return {
+            navbar: {
+                logo: "./img/Logo.png",
+                link: [
+                    "HOME",
+                    "BLOG",
+                    "EVENTS",
+                    "GALLERY",
+                    "ABOUT US",
+                    "CONTACT US",
+                    "SHOP",
+                ],
+
+            }
         }
     },
 })
